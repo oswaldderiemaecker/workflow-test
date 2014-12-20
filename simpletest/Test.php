86195d0d852ad19a58bb953fc33f75c9571c102e
Pascal Paulis <ppaulis@gmail.com>
2014-12-20 13:15:46 +0100

    added SimpleTest

---
+++ simpletest/Test.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once(dirname(__FILE__) . '/../vendor/simpletest/simpletest/autorun.php');
+
+class TestOfLogging extends UnitTestCase
+{
+    function testMethod1()
+    {
+        $this->assertTrue(true);
+    }
+}
