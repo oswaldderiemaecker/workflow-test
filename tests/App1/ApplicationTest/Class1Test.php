cd3677b83e8348437d3085db17157dcdf2d63432
ppaulis <pascal.paulis@continuousphp.com>
2016-01-18 11:42:30 +0100

    fixed tests

--- tests/App1/ApplicationTest/Class1Test.php
+++ tests/App1/ApplicationTest/Class1Test.php
@@ -4,12 +4,12 @@ namespace App1\ApplicationTest;
 
 class Class1Test extends \PHPUnit_Framework_TestCase
 {
-    /* @var $instance \Application\App1\Class1 */
+    /* @var $instance \App1\Class1 */
     protected $instance;
 
     public function setUp()
     {
-        $this->instance = new \Application\App1\Class1();
+        $this->instance = new \App1\Class1();
     }
 
     public function testMethod1()
