3bb51a5a0b2e814928cd5fd90d0a76c6ac4ba0d2
ppaulis <pascal.paulis@continuousphp.com>
2016-01-29 13:49:36 +0100

    fixed test

--- tests/unit/ExampleTest.php
+++ tests/unit/ExampleTest.php
@@ -19,9 +19,17 @@ class ExampleTest extends \Codeception\TestCase\Test
     // tests
     public function testMe()
     {
-        //$class1 = new \App1\Class1();
-        //$class1->method1();
+        $class1 = new TestClass();
+        $class1->method1();
         //$class1->method2();
         $this->assertTrue(true);
+    }
+}
+
+class TestClass
+{
+    public function method1()
+    {
+        $test = 123;
     }
 }
