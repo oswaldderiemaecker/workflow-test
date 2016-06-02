19ad9958aa6f7c81d553a48d9b55812b6a29c618
Oswald De Riemaecker <oswald@continuousphp.com>
2016-06-02 11:43:08 +0200

    Adding Selenium Tests

---
+++ tests/unit/GitHubFirefoxTest.php
@@ -0,0 +1,25 @@
+<?php
+class GitHubFirefoxTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * @var \RemoteWebDriver
+     */
+    protected $webDriver;
+
+    public function setUp()
+    {
+        $capabilities = array(\WebDriverCapabilityType::BROWSER_NAME => 'firefox');
+        $this->webDriver = RemoteWebDriver::create('http://localhost:4444/wd/hub', $capabilities);
+    }
+
+    protected $url = 'https://github.com';
+
+    public function testGitHubHomeFirefox()
+    {
+        $this->webDriver->get($this->url);
+        // checking that page title contains word 'GitHub'
+        $this->assertContains('GitHub', $this->webDriver->getTitle());
+    }
+
+}
+?>
